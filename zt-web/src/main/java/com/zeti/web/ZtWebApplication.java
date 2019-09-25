1024752d15dab01fc8e079322abe22ac69fd55d4
zeti <zmk95511@163.com>
2019-09-25 18:35:36 +0800

    项目文件引用

--- zt-web/src/main/java/com/zeti/web/ZtWebApplication.java
+++ zt-web/src/main/java/com/zeti/web/ZtWebApplication.java
@@ -1,4 +1,4 @@
-package com.zeti.ztweb;
+package com.zeti.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zt-web/src/test/java/com/zeti/web/ZtWebApplicationTests.java
+++ zt-web/src/test/java/com/zeti/web/ZtWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zeti.ztweb;
+package com.zeti.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
