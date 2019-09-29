c10532786064b95f17a01376d7f4c963a85f15a8
zeti <zmk95511@163.com>
2019-09-29 15:33:34 +0800

    代码生成完成

--- zt-mybatis-plus/src/main/java/com/mybatis/plus/base/BaseEntity.java
+++ zt-mybatis-plus/src/main/java/com/mybatis/plus/base/BaseEntity.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,38 +23,38 @@ public class BaseEntity implements Serializable {
      * 主键id
      */
     @TableId(value = "id", type = IdType.AUTO)
-//    @ApiModelProperty(value = "主键id")
+    @ApiModelProperty(value = "主键id")
     private Integer id;
 
     /**
      * 创建人
      */
-//    @ApiModelProperty(value = "创建人")
+    @ApiModelProperty(value = "创建人")
     private String createPerson;
     /**
      * 更新人
      */
-//    @ApiModelProperty(value = "更新人")
+    @ApiModelProperty(value = "更新人")
     private String updatePerson;
     /**
      * 创建时间
      */
-//    @DateTimeFormat(pattern = DateUtil.PATTERN_DATETIME)
-//    @JsonFormat(pattern = DateUtil.PATTERN_DATETIME)
-//    @ApiModelProperty(value = "创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
     /**
      * 更新时间
      */
-//    @DateTimeFormat(pattern = DateUtil.PATTERN_DATETIME)
-//    @JsonFormat(pattern = DateUtil.PATTERN_DATETIME)
-//    @ApiModelProperty(value = "更新时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新时间")
     private LocalDateTime updateTime;
     /**
      * 状态[0:未删除,1:删除]
      */
     @TableLogic
-//    @ApiModelProperty(value = "是否已删除")
+    @ApiModelProperty(value = "是否已删除")
     private Integer isDeleted;
 }
 
