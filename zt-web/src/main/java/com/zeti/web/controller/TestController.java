c45dc1b62120e4e953df2f63e1396a15680c040b
zeti <zmk95511@163.com>
2019-09-29 19:07:47 +0800

    commit 0.1

--- zt-web/src/main/java/com/zeti/web/controller/TestController.java
+++ zt-web/src/main/java/com/zeti/web/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.zeti.web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Description: (用一句话描述该文件做什么)
  *
  * @Date 2019/9/26 15:46
  * @Author Zeti
  */
+@RestController
 public class TestController {
 
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("测试-->：");
+        return "hello, controller";
+    }
 
 }
