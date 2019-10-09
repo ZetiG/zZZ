3cdd737ec86481c222185ad0dcd96963f88307ff
zeti <zmk95511@163.com>
2019-10-09 19:08:13 +0800

    gateway

---
+++ zt-gateway/src/main/java/com/zt/gateway/ZtGateWayApplication.java
@@ -0,0 +1,18 @@
+package com.zt.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: (用一句话描述该文件做什么)
+ *
+ * @Date 2019/10/8 17:06
+ * @Author Zeti
+ */
+@SpringBootApplication
+public class ZtGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZtGateWayApplication.class, args);
+    }
+
+}
---
+++ zt-gateway/src/main/java/com/zt/gateway/config/RouteConfig.java
@@ -0,0 +1,17 @@
+package com.zt.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description: (用一句话描述该文件做什么)
+ *
+ * @Date 2019/10/8 17:09
+ * @Author Zeti
+ */
+@Slf4j
+@Configuration
+public class RouteConfig {
+
+
+}
