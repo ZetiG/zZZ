49fc7c991c8f2d29cccaf0258f1175e5b366f5f8
zhangmk <zmk95511@163.com>
2020-04-20 23:40:03 +0800

    nacos

--- zt-common/src/test/java/com/zeti/common/CommonTest.java
+++ zt-common/src/test/java/com/zeti/common/CommonTest.java
@@ -3,7 +3,7 @@ package com.zeti.common;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openjdk.jol.info.ClassLayout;
+//import org.openjdk.jol.info.ClassLayout;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,10 +24,10 @@ public class CommonTest {
 
     @Test
     public void testMarkWord() {
-        String s = ClassLayout.parseInstance(CommonTest.s).toPrintable();
+//        String s = ClassLayout.parseInstance(CommonTest.s).toPrintable();
         log.info("mark word:{}", s);
 
-        String i = ClassLayout.parseInstance(CommonTest.i).toPrintable();
+//        String i = ClassLayout.parseInstance(CommonTest.i).toPrintable();
         log.info("mark word:{}", i);
     }
 
